65ddc3aa4e9a0784b5648f1cbf82b19506e11b4b
morphinchik <bykon.danil@gmail.com>
2021-01-15 14:23:16 +0200

    Add Bow and Arrow

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Arrow : XRGrabInteractable
+{
+    public float speed = 1000.0f;
+
+    public Transform tip = null;
+
+    private bool inAir = false;
+
+    private Vector3 lastPosition = Vector3.zero;
+
+    private Rigidbody rigidBody = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (inAir)
+        {
+            CheckForCollision();
+            lastPosition = tip.position;
+        }
+    }
+
+    private void CheckForCollision()
+    {
+        if (Physics.Linecast(lastPosition, tip.position))
+            Stop();
+    }
+
+    private void Stop()
+    {
+        inAir = false;
+        SetPhysics(false);
+        Destroy(gameObject, 2f);
+    }
+
+    public void Release(float pullValue)
+    {
+        inAir = true;
+        SetPhysics(true);
+
+        MaskAndFire(pullValue);
+        StartCoroutine(RotateWithVelocity());
+
+        lastPosition = tip.position;
+    }
+
+    private void SetPhysics(bool usePhysics)
+    {
+        rigidBody.isKinematic = !usePhysics;
+        rigidBody.useGravity = usePhysics;
+    }
+
+    private void MaskAndFire(float power)
+    {
+        // colliders[0].enabled = false;
+        interactionLayerMask = 1 << LayerMask.NameToLayer("Ignore");
+
+        Vector3 force = transform.forward * (power * speed);
+        rigidBody.AddForce(force);
+    }
+
+    private IEnumerator RotateWithVelocity()
+    {
+        yield return new WaitForFixedUpdate();
+        while (inAir)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(rigidBody.velocity, transform.up);
+            transform.rotation = newRotation;
+            yield return null;
+        }
+    }
+
+    public new void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        base.OnSelectEntered(interactor);
+    }
+
+    public new void OnSelectExiting(XRBaseInteractor interactor)
+    {
+        base.OnSelectExiting(interactor);
+    }
+
+    public new void OnSelectExited(XRBaseInteractor interactor)
+    {
+        base.OnSelectExited(interactor);
+    }
+}
---
+++ Assets/Scripts/Notch.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Notch : XRSocketInteractor
+{
+    private Puller puller = null;
+    private Arrow currentArrow = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        puller = GetComponent<Puller>();
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        puller.onSelectExited.AddListener(TryToReleaseArrow);
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        puller.onSelectExited.RemoveListener(TryToReleaseArrow); //onSelectEntered
+    }
+
+    protected override void OnSelectEntered(XRBaseInteractable interactable)
+    {
+        //base.OnSelectEntering(interactable);
+        base.OnSelectEntered(interactable);
+        StoreArrow(interactable);
+    }
+
+    private void StoreArrow(XRBaseInteractable interactable)
+    {
+        if (interactable is Arrow arrow)
+            currentArrow = arrow;
+    }
+
+    private void TryToReleaseArrow(XRBaseInteractor interactor)
+    {
+        if (currentArrow)
+        {
+            ForceDeselect();
+            ReleaseArrow();
+        }
+    }
+
+    private void ForceDeselect()
+    {
+
+        base.OnSelectExiting(currentArrow);
+        base.OnSelectExited(currentArrow);
+
+        currentArrow.OnSelectExiting(this);
+        currentArrow.OnSelectExited(this);
+
+        // base.OnSelectExited(currentArrow);
+        //currentArrow.OnSelectExited(this);
+    }
+
+    private void ReleaseArrow()
+    {
+        currentArrow.Release(puller.PullAmount);
+        currentArrow = null;
+    }
+
+    public override XRBaseInteractable.MovementType? selectedInteractableMovementTypeOverride
+    {
+        get { return XRBaseInteractable.MovementType.Instantaneous; }
+    }
+}
